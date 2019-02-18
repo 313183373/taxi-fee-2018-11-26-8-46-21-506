c5129a396ead8a0ce32036858d8408981dce91aa
xion <313183373@qq.com>
2019-02-18 21:11:49 +0800

    refactor test: "should return 5 when miles is 5 and waiting time is 4"

--- main/main.js
+++ main/main.js
@@ -1,5 +1,6 @@
 const MILE_PRICE = 0.8;
+const WAITING_TIME_PRICE = 0.25;
 
 module.exports = function main(miles = 0, waitingTime = 0) {
-    return miles * MILE_PRICE + waitingTime * 0.25;
+    return miles * MILE_PRICE + waitingTime * WAITING_TIME_PRICE;
 };
