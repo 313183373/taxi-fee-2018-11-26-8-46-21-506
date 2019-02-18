d43070503d75f9db17b9ec7ebac3382b55f27f66
xion <313183373@qq.com>
2019-02-18 21:13:18 +0800

    pass test: "should return 5 when miles is 5 and waiting time is 3"

--- main/main.js
+++ main/main.js
@@ -2,5 +2,5 @@ const MILE_PRICE = 0.8;
 const WAITING_TIME_PRICE = 0.25;
 
 module.exports = function main(miles = 0, waitingTime = 0) {
-    return miles * MILE_PRICE + waitingTime * WAITING_TIME_PRICE;
+    return Math.round(miles * MILE_PRICE + waitingTime * WAITING_TIME_PRICE);
 };
