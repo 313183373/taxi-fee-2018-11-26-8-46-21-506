010a1f470cb81aa43229c37e62498e2bcf5fd76f
xion <313183373@qq.com>
2019-02-18 21:12:35 +0800

    add test: "should return 5 when miles is 5 and waiting time is 3"

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -9,4 +9,8 @@ describe('taxi fee', function () {
     it('should return 5 when miles is 5 and waiting time is 4', function () {
         assert.deepEqual(main(5, 4), 5);
     });
+
+    it('should return 5 when miles is 5 and waiting time is 3', function () {
+        assert.deepEqual(main(5,3), 5);
+    });
 });
