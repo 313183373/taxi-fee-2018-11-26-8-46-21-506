6ac510c3030e3e260f91a73c5dd7f1f3aefbb9a3
xion <313183373@qq.com>
2019-02-18 21:26:13 +0800

    correction, i forgot the start price

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,15 +2,15 @@ const main = require('../main/main');
 const assert = require('assert').strict;
 
 describe('taxi fee', function () {
-    it('should return 4 when miles is 5 and no waiting time', function () {
-        assert.deepEqual(main(5), 4);
+    it('should return 8 when miles is 5 and no waiting time', function () {
+        assert.deepEqual(main(5), 8);
     });
 
     it('should return 5 when miles is 5 and waiting time is 4', function () {
-        assert.deepEqual(main(5, 4), 5);
+        assert.deepEqual(main(5, 4), 9);
     });
 
     it('should return 5 when miles is 5 and waiting time is 3', function () {
-        assert.deepEqual(main(5,3), 5);
+        assert.deepEqual(main(5, 5), 10);
     });
 });
