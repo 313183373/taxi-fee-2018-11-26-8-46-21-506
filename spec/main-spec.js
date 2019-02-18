0835e9ee1e196ee104499d3be080a1832a80ef22
xion <313183373@qq.com>
2019-02-18 21:06:25 +0800

    add test: "should return 4 when miles is 5 and no waiting time"

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,8 @@
 const main = require('../main/main');
+const assert = require('assert').strict;
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('should return 4 when miles is 5 and no waiting time', function () {
+        assert.deepEqual(main(5), 4);
+    });
 });
