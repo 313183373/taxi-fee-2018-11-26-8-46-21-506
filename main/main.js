6e8edf3e13417944202ba1623c711ab5f1ad288e
xion <313183373@qq.com>
2019-02-18 21:32:33 +0800

    refactor test: "should return 6 when miles is 2 and waiting time is 4"

--- main/main.js
+++ main/main.js
@@ -1,6 +1,8 @@
 const MILE_PRICE = 0.8;
 const WAITING_TIME_PRICE = 0.25;
+const START_MILES_LIMIT = 2;
+const START_PRICE = 6;
 
 module.exports = function main(miles = 0, waitingTime = 0) {
-    return Math.round((miles <= 2 ? 6 : 6 + (miles - 2) * MILE_PRICE) + waitingTime * WAITING_TIME_PRICE);
+    return Math.round((miles <= START_MILES_LIMIT ? START_PRICE : START_PRICE + (miles - START_MILES_LIMIT) * MILE_PRICE) + waitingTime * WAITING_TIME_PRICE);
 };
