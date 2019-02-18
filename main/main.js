831e648159e569f539c03794b6b191cb892ebe06
xion <313183373@qq.com>
2019-02-18 21:45:20 +0800

    pass test: "should return 13 when miles is 10 and no waiting time"

--- main/main.js
+++ main/main.js
@@ -4,5 +4,9 @@ const START_MILES_LIMIT = 2;
 const START_PRICE = 6;
 
 module.exports = function main(miles = 0, waitingTime = 0) {
-    return Math.round((miles <= START_MILES_LIMIT ? START_PRICE : START_PRICE + (miles - START_MILES_LIMIT) * MILE_PRICE) + waitingTime * WAITING_TIME_PRICE);
+    return Math.round((miles <= START_MILES_LIMIT ?
+        START_PRICE : START_PRICE + (miles > 8 ?        // start price
+        ((8 - START_MILES_LIMIT) * MILE_PRICE + (miles - 8) * MILE_PRICE * (1 + 0.5)) :        //miles price(miles > 8)
+        ((miles - START_MILES_LIMIT) * MILE_PRICE))) +      //miles price(miles <= 8)
+        waitingTime * WAITING_TIME_PRICE);          //waiting time price
 };
